1da26c93ab181ecb4016e6898eed78fa1695979d
Asthestarsfalll <1186454801@qq.com>
2022-12-27 15:26:28 +0800

    add powershell update script

---
+++ update.ps1
@@ -0,0 +1,13 @@
+echo "Update config of LunarVIM"
+
+cp -r -force ~/.local/bin/*.ps1 ./LunarVim/Window11/
+cp -force $env:LUNARVIM_CONFIG_DIR/* ./LunarVim/Window11/lvim/
+
+echo "Update config of powershell"
+
+cp -force $PROFILE ./powershell/
+
+
+echo "Update config oh-my-posh"
+
+cp -force $env:POSH_THEMES_PATH\M365* ./oh-my-posh/
